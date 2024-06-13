b460926b1b38333fba7285c089e1983f709485af
DESKTOP-O4NN41P\82109 <khb4130@naver.com>
2024-06-13 16:27:01 +0900

    BCrypt 암호화 메서드 설정

--- src/main/java/com/aloha/security/config/SecurityConfig.java
+++ src/main/java/com/aloha/security/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean //시큐리티 암호화
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,4 +38,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 }
